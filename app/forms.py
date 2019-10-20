cd3f1045a0b89c146d63763ff7cda565340f5bb4
Meirzv <mz2530@nyu.edu>
2019-10-20 11:20:19 -0400

    Adding spell_check file

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, IntegerField
-# from .util.validators import Unique
-# from app.views import programMemory
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',  [validators.input_required()])
+    username = StringField('Username', [validators.input_required()])
     password = PasswordField('Password', [validators.input_required()])
-    multifa  = IntegerField("2FA - Phone Number")
-    remember_me = BooleanField('Remember Me')
+    multifa = IntegerField("2FA - Phone Number", [validators.optional()])
+    remember_me = BooleanField('Remember Me', [validators.optional()])
     submit = SubmitField('Sign In')
 
+
 class RegisterForm(FlaskForm):
-    username = StringField('Username',  [validators.input_required()])
+    username = StringField('Username', [validators.input_required()])
     password = PasswordField('Password', [validators.input_required()])
-    multifa  = IntegerField("2FA - Phone Number", [validators.optional()])
+    multifa = IntegerField("2FA - Phone Number", [validators.optional()])
     submit = SubmitField('Sign Up')
+
+
+class SpellChecker(FlaskForm):
+    command = StringField('Spell check words input', [validators.input_required()])
--- app/views.py
+++ app/views.py
@@ -1,16 +1,24 @@
 from flask import render_template, flash, redirect, request
 from app import app
-from app.forms import LoginForm, RegisterForm
+from app.forms import LoginForm, RegisterForm, SpellChecker
 
-programMemory = dict()  #
+programMemory = dict()  # [key = lower key username. value = 1) password, 2) original username, 3) 2fa]
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        if form.username.data.lower() in programMemory.keys():
+            if form.password.data == programMemory[form.username.data.lower()][0]:
+                flash("Login is successful ")
+                return redirect('/spell_check')
+            else:
+                flash("Password is incorrect. Please try again")
+                return redirect('/login')
+        else:
+            flash("Username is incorrect. Please try again")
+            return redirect('/login')
         return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
 
@@ -20,16 +28,22 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.username.data.lower() in programMemory.keys():
-            flash("Username must be unique. '" + form.username.data+"' is not available. ")
+            flash("Username must be unique. '" + form.username.data + "' is not available. ")
             return redirect('/register')
         else:
-            programMemory[form.username.data.lower()] = [form.password.data,form.username.data]
+            programMemory[form.username.data.lower()] = [form.password.data, form.username.data, form.multifa.data]
             flash("Successful")
             for element in programMemory.keys():
                 print(element)
             return redirect('/index')
     else:
         return render_template('register.html', title='Sign Up', form=form)
+
+
+@app.route('/spell_check')
+def spell_checker():
+    form = SpellChecker()
+    return render_template('spell_check.html', title="Spell Check App", form=form)
 
 
 @app.route('/index')
